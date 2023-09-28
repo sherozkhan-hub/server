2830e9ce97689a61172bc85b7007a0f14f80ed02
unknown <sherozkhan321@gmail.com>
2023-09-28 23:06:23 +0500

    errors fixed

--- api/helpers/sendEmail.js
+++
@@ -1,7 +0,0 @@
-export const resetPasswordLink = async (user, res) => {
-  const { email, _id } = user;
-
-  const token = _id + uuidv4();
-
-  const link = APP_URL + "users/reset-password" + _id + "/" + token;
-};
